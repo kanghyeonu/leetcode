d7c24ade9299d1fdb7c687da3d4f54c323888004
KHW <51577531+kanghyeonu@users.noreply.github.com>
2024-10-07 14:21:38 +0900

    Time: 58 ms (29.31%), Space: 17 MB (11.51%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,17 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        asc = list(range(numRows))
+        des = list(range(numRows-2, 0, -1))
+        seq = asc + des
+
+        l = ["" for _ in range(numRows)]
+
+        for i, char in enumerate(s):
+            l[seq[i % len(seq)]] += char
+        
+        ans = ""
+        for chars in l:
+            ans += chars
+
+        return ans
+        
