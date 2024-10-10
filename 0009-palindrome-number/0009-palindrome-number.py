a027b07028ef7d61408b2244a3f57a3defebec4a
KHW <51577531+kanghyeonu@users.noreply.github.com>
2024-10-10 13:35:27 +0900

    Time: 49 ms (76.02%), Space: 16.6 MB (11.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,10 +3,6 @@ class Solution:
         if x < 0:
             return False
         
-        l = list(str(x))
+        s = str(x)
 
-        for i in range(len(l)//2):
-            if l[i] != l[-1*i - 1]:
-                return False
-        
-        return True
+        return s == s[::-1]
