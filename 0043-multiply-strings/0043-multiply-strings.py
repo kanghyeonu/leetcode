dde01888ecbe60645e6513e165ca799d1f496045
KHW <51577531+kanghyeonu@users.noreply.github.com>
2024-11-20 11:21:48 +0900

    Time: 77 ms (12.67%), Space: 16.9 MB (7.04%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,30 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num1, num2 = int(num1), int(num2)
-        return str(num1*num2)
+        answer = 0
+        result = []
+
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+
+        for n2 in num2:
+            carry = 0
+            digit = []
+            for n1 in num1:
+                curr = (int(n1) * int(n2)) + carry
+                digit.append(curr%10)
+                carry = curr // 10
+
+            if carry != 0:
+                digit.append(carry)
+                
+            sum_ = 0
+            for i in range(len(digit)):
+                sum_ += digit[i] * (10**i)
+
+            result.append(sum_)
+
+        for i in range(len(result)):
+            curr = result[i] * (10**i)
+            answer += curr
+
+        return str(answer)
