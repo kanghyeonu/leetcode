90a9005d00f1353bfb0fcc7e1ca2eb27dacf8635
KHW <51577531+kanghyeonu@users.noreply.github.com>
2024-11-22 12:51:08 +0900

    Time: 2162 ms (22.3%), Space: 17.7 MB (34.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,12 +3,10 @@ class Solution:
         dp = [0 for _ in range(len(nums))]
 
         for i in range(len(nums)-1):
-            for j in range(nums[i]+1):
-                if i + j > len(nums)-1 or j == 0:
+            for j in range(1, nums[i]+1):
+                if i + j > len(nums)-1:
                     continue
                 if dp[i+j] == 0:
-                    dp[i+j] = dp[i] + 1
-                elif dp[i+j] > dp[i] + 1:
                     dp[i+j] = dp[i] + 1
                 
         return dp[-1]
