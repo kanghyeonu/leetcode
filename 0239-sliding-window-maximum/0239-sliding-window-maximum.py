492aeba6db11700fc0ff4d3219b443b57a8ad4e3
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-06-17 10:52:13 +0900

    Time: 126 ms (98.46%), Space: 30.9 MB (86.98%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,23 +1,29 @@
 class Solution:
+    
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        
-        hq = [-i for i in nums[:k]]
-        heapq.heapify(hq)
-        removals = []
-        answer = [-hq[0]]
 
-        for i in range(1, len(nums) - k + 1):
-            if nums[i-1] == -hq[0]:
-                heapq.heappop(hq)
-                while removals and hq[0] == removals[0]:
-                    heapq.heappop(hq)
-                    heapq.heappop(removals)
-            
-            else:
-                heapq.heappush(removals, -nums[i-1])
-            
-            heapq.heappush(hq, -nums[i+k-1])
-            answer.append(-hq[0])
+        dq = deque()
+        res = []
 
-        return answer
+        for i in range(k):
+            while dq and nums[i] >= nums[dq[-1]]: 
+                dq.pop()
             
+            dq.append(i)
+
+        res.append(nums[dq[0]])
+
+        for i in range(k, len(nums)):
+
+            if dq and dq[0] == i - k:
+                dq.popleft()
+            
+            while dq and nums[i] >= nums[dq[-1]]: 
+                dq.pop() 
+            
+            dq.append(i)
+            
+            res.append(nums[dq[0]])
+
+        return res
+
